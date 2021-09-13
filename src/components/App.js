0eaf67246c8f46f254a7812d91e7953cfb7f793f
stanislavkononiuk <stanislavkononiuk@gmail.com>
2021-09-13 10:18:37 +0100

    Add Files structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
