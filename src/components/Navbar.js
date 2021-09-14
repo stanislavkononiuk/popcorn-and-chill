e67330e1ba1d528a1447df33e63cb4dc1c1d9367
stanislavkononiuk <stanislavkononiuk@gmail.com>
2021-09-14 11:36:29 +0100

    Add changes to navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav>
+    <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '10px 5px' }}>
       <div>logo</div>
       <form>
         <input type="text" placeholder="search movie" />
         <button type="submit">search</button>
       </form>
+      <select>
+        <option>Upcoming</option>
+        <option>Top rated</option>
+      </select>
     </nav>
   );
 }
