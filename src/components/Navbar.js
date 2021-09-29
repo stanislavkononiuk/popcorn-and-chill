e082d21b47b80f87b6bc35ca531182a75d8b2e66
stanislavkononiuk <stanislavkononiuk@gmail.com>
2021-09-29 15:12:17 +0100

    Implement handleSearch and handleSubmit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { popular, valueScore } from '../actions/index';
@@ -6,10 +6,11 @@ import { popularMovies } from '../API/API';
 import popcorn from '../images/popcorn.png';
 
 function Navbar() {
+  const [searchValue, setSearchValue] = useState('');
   const dispatch = useDispatch();
   const rating = useSelector((state) => state.scoreReducer.score);
 
-  function setMoviesRating(event) {
+  function handleRating(event) {
     const { value } = event.target;
     if (value === 'choose a rating') return;
     dispatch(valueScore(value));
@@ -18,16 +19,24 @@ function Navbar() {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleSearch(event) {
+    const { value } = event.target;
+    setSearchValue(value);
+  }
   return (
     <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '10px 5px' }}>
       <div>
         <Link to="/"><img style={{ height: '60px' }} src={popcorn} alt="logo" /></Link>
       </div>
-      <form>
-        <input type="text" placeholder="search movie" />
+      <form onSubmit={handleSubmit}>
+        <input value={searchValue} onChange={handleSearch} type="text" placeholder="search movie" />
         <button type="submit">search</button>
       </form>
-      <select value={rating} onChange={setMoviesRating}>
+      <select value={rating} onChange={handleRating}>
         <option value="choose a rating">choose a rating</option>
         <option value={9}>9+</option>
         <option value={8}>8+</option>
