34b8acf9d8246325fe4f49a91753d57db24a0dc5
stanislavkononiuk <stanislavkononiuk@gmail.com>
2021-10-05 11:20:35 +0100

    Change Movie component from regular function to an arrow function

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,21 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCSS from './Movie.module.css';
 
-function Movie({ imageScr, score, title }) {
-  return (
-    <div className={MovieCSS.movieWraper}>
-      <img className={MovieCSS.image} src={imageScr} alt={title} />
-      <div className={MovieCSS.infoWraper}>
-        <h2 className={MovieCSS.header}>{title}</h2>
-        <p className={MovieCSS.score}>
-          score:
-          {' '}
-          {score}
-        </p>
-      </div>
+const Movie = ({ imageScr, score, title }) => (
+  <div className={MovieCSS.movieWraper}>
+    <img className={MovieCSS.image} src={imageScr} alt={title} />
+    <div className={MovieCSS.infoWraper}>
+      <h2 className={MovieCSS.header}>{title}</h2>
+      <p className={MovieCSS.score}>
+        score:
+        {' '}
+        {score}
+      </p>
     </div>
-  );
-}
+  </div>
+);
 
 Movie.propTypes = {
   imageScr: PropTypes.string.isRequired,
